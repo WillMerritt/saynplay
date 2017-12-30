4bb4a6851aa8e831ff5a33ac5dd41c140bd601ac
nelsondude <alexn1336@gmail.com>
2017-12-29 21:39:21 -0600

    My first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/board/board.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+  rows = new Array(8);
+
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}
