be010ae31fe9395a56a95618c4878c8691ee7c20
nelsondude <alexn1336@gmail.com>
2018-01-21 09:59:17 -0500

    Changed hoerku port

--- server.js
+++ server.js
@@ -170,8 +170,8 @@ const updateGameById = function(db, id, board, callback) {
 };
 
 
-http.listen( 3000, () => {
-  console.log('listening to port 3000')
+http.listen( process.env.PORT || 8000, () => {
+  console.log('listening to port 8000')
 });
 
 
@@ -192,59 +192,4 @@ Emit the fetched board to both sockets in the group
 
 
 */
-
-
-
-// TESTING
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('documents');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-};
-
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('documents');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs);
-    callback(docs);
-  });
-};
-
-const updateDocument = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('documents');
-  // Update document where a is 2, set b equal to 1
-  collection.updateOne({ a : 2 }
-    , { $set: { b : 1 } }, function(err, result) {
-      assert.equal(err, null);
-      assert.equal(1, result.result.n);
-      console.log("Updated the document with the field a equal to 2");
-      callback(result);
-    });
-};
-
-const removeDocument = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('documents');
-  // Delete document where a is 3
-  collection.deleteOne({ a : 3 }, function(err, result) {
-    assert.equal(err, null);
-    assert.equal(1, result.result.n);
-    console.log("Removed the document with the field a equal to 3");
-    callback(result);
-  });
-};
 
